9aff3e82f01924645c784497361400f540e75a1e
SaintBanan <gumnew@mail.ru>
2020-12-12 01:24:55 +0700

    Модифицировано DetailActivity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -2,33 +2,51 @@ package com.udacity.sandwichclub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Collection;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    ImageView ingredientsIv;
+    TextView description, also_known_as, place_of_origin, ingredients;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ingredientsIv = findViewById(R.id.image_iv);
+        description = findViewById(R.id.description_tv);
+        also_known_as = findViewById(R.id.also_known_tv);
+        place_of_origin = findViewById(R.id.origin_tv);
+        ingredients = findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
+
         if (intent == null) {
             closeOnError();
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
@@ -36,13 +54,19 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-        String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        String sandwich_json = sandwiches[position];
+        Sandwich sandwich = JsonUtils.parseSandwichJson(sandwich_json);
+
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
+
+        description.setText(sandwich.getDescription());
+        place_of_origin.setText(sandwich.getPlaceOfOrigin());
+        also_known_as.setText(sandwich.getAlsoKnownAs().toString().replaceAll("[\\]\\[]", ""));
+        ingredients.setText(sandwich.getIngredients().toString().replaceAll("[\\]\\[]", ""));
 
         populateUI();
         Picasso.with(this)
